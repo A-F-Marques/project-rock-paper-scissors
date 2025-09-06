285e45a34197d73c14f0572936b4ee12dcd983b0
A-F_Marques <andrefmarques99@gmail.com>
2025-09-06 08:15:10 +0100

    .

--- script.js
+++ script.js
@@ -10,7 +10,26 @@
 
     let computerInput = 0;
     // create variable computerInput with value of 0
+    const body = document.querySelector("body");
+    const result = document.createElement("div");
     
+
+    const rockBtn = document.querySelector("#rock");
+    rockBtn.addEventListener(("click"), (event) => {
+        return userInput = "rock", playGame();
+    })
+    
+    const paperBtn = document.querySelector("#paper")
+    paperBtn.addEventListener(("click"), () => {
+        return userInput = "paper", playGame();
+    })
+
+    const scissorsBtn = document.querySelector("#scissors")
+    scissorsBtn.addEventListener(("click"), () => {
+        return userInput = "scissors", playGame();
+    })
+
+   
 
     function getComputerInput() {
 
@@ -31,13 +50,12 @@
     //     ELSE() => computerInput = Scissors;}
 
 
+               /* function getUserInput() {
 
-    function getUserInput() {
-
-        userInput = prompt("rock, paper or scissors?");
-        userInput = userInput.toLowerCase();
-        return userInput
-    }
+                    userInput = prompt("rock, paper or scissors?");
+                    userInput = userInput.toLowerCase();
+                    return userInput
+                }*/
      // create function named getUserInput in which {
         // userInput = prompt(rock, paper, scissors)
         // convert userInput to lowercase
@@ -47,7 +65,6 @@ function playGame() {
 
         getComputerInput();
         console.log(computerInput);
-        getUserInput();
         console.log(userInput);
 
         function playRound(userInput, computerInput) {
@@ -66,40 +83,38 @@ function playGame() {
      const result = playRound(userInput, computerInput)
         if (result === "user") {
             userScore = (userScore + 1);
-            return userScore
+            return userScore, updateScore();
         }
         if ( result === "computer") {
             computerScore = (computerScore + 1);
-            return computerScore
-        }
+            return computerScore, updateScore();
+        } 
+        else updateScore()
+
 }
-// create function named playGame in which {
-// get user input by running getUserInput
-// get computer input by running getComputerInput
-// play one round by running playRound with userInput and computerInput
-// if user wins the round, increment userScore by 1
-// else if computer wins the round, increment computerScore by 1
-// else (tie), do not change scores
-
- for (let i = 0; i < 5 ; i++) {
-        playGame()
- }
-// Creates the loop that makes the playGame function run 5 times
+function updateScore() {
+    result.textContent = "The user Has " + userScore + " points.  The computer has " + computerScore + " points"
+    result.setAttribute("style", "color: black; background: lightGray; height: 100px;");
+    result.style.textAlign = "center"
+    result.style.verticalAlign = "auto"
+   
+    return body.appendChild(result);
+}
 
 
- function winner() {
-    if (computerScore > userScore) {
-        return "The computer won this game!!";
-    }
-    else if (userScore > computerScore) {
-        return "You won this game!!";
-    }
-    else {
-        return "It was a Tie!!"
-    }
- }
- // Function for revealing the winner based on userScore and computerScore
+                       /* function winner() {
+                            if (computerScore > userScore) {
+                                return "The computer won this game!!";
+                            }
+                            else if (userScore > computerScore) {
+                                return "You won this game!!";
+                            }
+                            else {
+                                return "It was a Tie!!"
+                            }
+                        }
+                        // Function for revealing the winner based on userScore and computerScore
 
- console.log("The user score is: " + userScore + " and the computer score is: " + computerScore + " therefore...")
- console.log(winner())
-  // Write in console the score of each and the result of it through the Winner() function
+                        console.log("The user score is: " + userScore + " and the computer score is: " + computerScore + " therefore...")
+                        console.log(winner())
+                        // Write in console the score of each and the result of it through the Winner() function*/
